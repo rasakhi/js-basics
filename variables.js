ee4375582db147e2d95282f9f830d91484a722c7
Michael Achumba <michaelachumba@gmail.com>
2025-07-14 23:12:41 +0100

    Add 3 introductory JS scripts

---
+++ variables.js
@@ -0,0 +1,23 @@
+let name = "Michael";
+console.log(typeof name, name);
+
+let age = 31;
+console.log(typeof age, age);
+
+let isStudent = true;
+console.log(typeof isStudent, isStudent);
+
+const address = {
+    city : "Abuja",
+    country: "Nigeria"
+};
+console.log(typeof address, address)
+
+const hobbies = ["Football", "Music", "Songwriting"];
+console.log(typeof hobbies, hobbies);
+
+let undefinedVariable;
+console.log(typeof undefinedVariable, undefinedVariable);
+
+let nullVariable = null;
+console.log(typeof nullVariable, nullVariable);
