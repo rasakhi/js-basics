01fe2fb4623af0fa276e4d84f45661923e8e0b9c
Michael Achumba <michaelachumba@gmail.com>
2025-07-14 23:49:10 +0100

    Add 3 scripts that explore loops, conditional statements, and functions

---
+++ numbers.js
@@ -0,0 +1,9 @@
+let number = 5;
+
+if (number > 0) {
+    console.log(`${number} is positive`);
+} else if (number < 0) {
+    console.log(`${number} is negative`);
+} else {
+    console.log(`${number} is zero`);
+}
---
+++ sum.js
@@ -0,0 +1,6 @@
+function sum(num1, num2) {
+    return num1 + num2;
+}
+
+result = sum(2, 3);
+console.log(result);
